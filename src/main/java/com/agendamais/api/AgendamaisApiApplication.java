da9de779b0226f7d606d13d9edf57c3c0b22a56a
Thiago Henrique Fragata <thiago.henrique.fragata@hotmail.com>
2025-01-23 18:06:12 -0400

    feat: create project

---
+++ src/main/java/com/agendamais/api/AgendamaisApiApplication.java
@@ -0,0 +1,13 @@
+package com.agendamais.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamaisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamaisApiApplication.class, args);
+	}
+
+}
