3a17c227bb5a222723aa8b77ba32854f65a9f74a
Thiago Henrique Fragata <thiago.henrique.fragata@hotmail.com>
2025-02-06 03:43:23 -0400

    feat: add swagger config in employee_controller

--- src/main/java/com/agendamais/api/controllers/employee_controller.java
+++ src/main/java/com/agendamais/api/controllers/employee_controller.java
@@ -4,15 +4,19 @@ import com.agendamais.api.dtos.employee.*;
 import com.agendamais.api.models.employee_model;
 import com.agendamais.api.services.employee_service;
 import com.agendamais.api.config.success_response_config;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Tag(name = "Funcionários", description = "Endpoints de funcionário")
 @RestController
 @RequestMapping("employees")
 public class employee_controller {
@@ -21,6 +25,8 @@ public class employee_controller {
     private employee_service employee_service;
 
 
+    @Operation(summary = "Criar um funcionário pelo ID do usuário")
+    @PreAuthorize("hasAnyRole('ADMIN', 'OWNER')")
     @PostMapping("/with-existing-user")
     public ResponseEntity<employee_response_record_dto> create_employee_with_existing_user(
             @RequestBody @Valid employee_create_with_existing_user_record_dto employee_dto) {
@@ -31,6 +37,8 @@ public class employee_controller {
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
 
+    @Operation(summary = "Criar um funcionário")
+    @PreAuthorize("hasAnyRole('ADMIN', 'OWNER')")
     @PostMapping
     public ResponseEntity<employee_response_record_dto> create_employee_with_user(
             @RequestBody @Valid employee_create_with_user_record_dto employee_dto) {
@@ -41,6 +49,8 @@ public class employee_controller {
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
 
+    @Operation(summary = "Buscar todos os funcionários")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping
     public ResponseEntity<List<employee_list_response_record_dto>> get_all_employees() {
         List<employee_model> employees = employee_service.get_all_employees();
@@ -50,6 +60,8 @@ public class employee_controller {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Buscar funcionários pelo ID da loja")
+    @PreAuthorize("hasAnyRole('ADMIN', 'OWNER', 'EMPLOYEE', 'CUSTOMER')")
     @GetMapping("/store/{storeId}")
     public ResponseEntity<List<employee_list_response_record_dto>> get_employees_by_store(@PathVariable Long storeId) {
         List<employee_model> employees = employee_service.get_employees_by_store(storeId);
@@ -61,6 +73,8 @@ public class employee_controller {
                 : ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Buscar funcionário pelo ID")
+    @PreAuthorize("hasAnyRole('ADMIN', 'OWNER', 'EMPLOYEE')")
     @GetMapping("/{id}")
     public ResponseEntity<employee_list_response_record_dto> get_employee_by_id(@PathVariable Long id) {
         employee_model employee = employee_service.get_employee_by_id(id);
@@ -68,6 +82,8 @@ public class employee_controller {
         return ResponseEntity.ok(responseDto);
     }
 
+    @Operation(summary = "Atualizar funcionário pelo ID")
+    @PreAuthorize("hasAnyRole('ADMIN', 'OWNER', 'EMPLOYEE')")
     @PutMapping("/{id}")
     public ResponseEntity<employee_update_response_record_dto> update_employee(
             @PathVariable Long id,
@@ -80,6 +96,8 @@ public class employee_controller {
         return ResponseEntity.ok(responseDto);
     }
 
+    @Operation(summary = "Desativar um funcionário")
+    @PreAuthorize("hasAnyRole('ADMIN', 'OWNER')")
     @DeleteMapping("/{id}")
     public ResponseEntity<success_response_config> deactivate_employee(@PathVariable Long id) {
         employee_service.deactivate_employee(id);
@@ -87,6 +105,8 @@ public class employee_controller {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Ativar um funcionário")
+    @PreAuthorize("hasAnyRole('ADMIN', 'OWNER')")
     @PutMapping("/{id}/activate")
     public ResponseEntity<success_response_config> activate_employee(@PathVariable Long id) {
         employee_service.activate_employee(id);
